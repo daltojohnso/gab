83fdb03dd4ba056c70c0ec42fd219abc95d6249d
Dalton Johnson <dalton@pendo.io>
2018-07-31 07:25:45 -0400

    Add firebase-config back

---
+++ src/firebase-config.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: 'AIzaSyAEHr9TDDkI04zcogtufsuKxYtnXwUNkAQ',
+    authDomain: 'gab-gab.firebaseapp.com',
+    databaseURL: 'https://gab-gab.firebaseio.com',
+    projectId: 'gab-gab',
+    storageBucket: 'gab-gab.appspot.com',
+    messagingSenderId: '462013320780'
+};
